661c59993341457745d99bb585d71892c81b3d2f
thealing <159199097+thealing@users.noreply.github.com>
2024-12-09 17:41:43 +0100

    Fixed D3D9 Hook Target Resizing

--- Hook/main.cpp
+++ Hook/main.cpp
@@ -26,6 +26,8 @@ struct OpenGL_Data {
 struct D3D9_Data {
 	HANDLE init_event;
 	HANDLE frame_event;
+	uint8_t* reset;
+	uint8_t* release;
 	uint8_t* present;
 	IDirect3DDevice9* device;
 	IDirect3DSurface9* back_buffer;
@@ -33,6 +35,7 @@ struct D3D9_Data {
 	IDirect3DSurface9* capture_buffer;
 	D3DLOCKED_RECT rect;
 	bool ready;
+	int ref_count;
 };
 
 struct D3D10_Data {
@@ -63,6 +66,12 @@ bool once() {
 	return InterlockedCompareExchange(&a, 1, 0) == 0;
 }
 
+void safe_release(IUnknown* unknown) {
+	if (unknown != NULL) {
+		unknown->Release();
+	}
+}
+
 BOOL opengl_do_swap_buffers(OpenGL_Data* data, HDC hdc) {
 	while (once()) {
 		glGetIntegerv(GL_VIEWPORT, data->viewport);
@@ -78,9 +87,42 @@ BOOL opengl_do_swap_buffers(OpenGL_Data* data, HDC hdc) {
 	return result;
 }
 
+int d3d9_get_reference_count(D3D9_Data* data, IDirect3DDevice9* device) {
+	int count = device->AddRef() - 1;
+	((ULONG (WINAPI *)(IDirect3DDevice9*))data->release)(device);
+	return count;
+}
+
+void d3d9_reset(D3D9_Data* data) {
+	data->device = NULL;
+	safe_release(data->back_buffer);
+	safe_release(data->middle_buffer);
+	safe_release(data->capture_buffer);
+	SetEvent(data->frame_event);
+	data->ref_count = 0;
+}
+
+ULONG d3d9_do_release(D3D9_Data* data, IDirect3DDevice9* device) {
+	ULONG result = ((ULONG (WINAPI *)(IDirect3DDevice9*))data->release)(device);
+	if (data->device == device && data->ref_count == d3d9_get_reference_count(data, device)) {
+		d3d9_reset(data);
+		return 0;
+	}
+	return result;
+}
+
+HRESULT d3d9_do_reset(D3D9_Data* data, IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* arg_1) {
+	if (data->device == device) {
+		d3d9_reset(data);
+		return 0;
+	}
+	return ((HRESULT (WINAPI *)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*))data->reset)(device, arg_1);
+}
+
 HRESULT d3d9_do_present(D3D9_Data* data, IDirect3DDevice9* device, const RECT* arg_1, const RECT* arg_2, HWND arg_3, const RGNDATA* arg_4) {
-	while (once()) {
+	while (data->device == NULL) {
 		data->device = device;
+		int ref_count_before = d3d9_get_reference_count(data, device);
 		if (FAILED(data->device->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &data->back_buffer))) {
 			break;
 		}
@@ -96,6 +138,8 @@ HRESULT d3d9_do_present(D3D9_Data* data, IDirect3DDevice9* device, const RECT* a
 		if (FAILED(data->device->CreateOffscreenPlainSurface(sd.Width, sd.Height, sd.Format, D3DPOOL_SYSTEMMEM, &data->capture_buffer, NULL))) {
 			break;
 		}
+		int ref_count_after = d3d9_get_reference_count(data, device);
+		data->ref_count = ref_count_after - ref_count_before;
 		SetEvent(data->init_event);
 	}
 	HRESULT result = ((HRESULT (WINAPI *)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*))data->present)(device, arg_1, arg_2, arg_3, arg_4);
@@ -190,6 +234,14 @@ BOOL WINAPI opengl_fake_swap_buffers(HDC hdc) {
 	return opengl_do_swap_buffers(&opengl_data, hdc);
 }
 
+ULONG WINAPI d3d9_fake_release(IDirect3DDevice9* device) {
+	return d3d9_do_release(&d3d9_data, device);
+}
+
+HRESULT  WINAPI d3d9_fake_reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* arg_1) {
+	return d3d9_do_reset(&d3d9_data, device, arg_1);
+}
+
 HRESULT WINAPI d3d9_fake_present(IDirect3DDevice9* device, const RECT* arg_1, const RECT* arg_2, HWND arg_3, const RGNDATA* arg_4) {
 	return d3d9_do_present(&d3d9_data, device, arg_1, arg_2, arg_3, arg_4);
 }
@@ -255,41 +307,50 @@ void opengl_proc(OpenGL_Data* data) {
 }
 
 void d3d9_proc(D3D9_Data* data) {
-	WaitForSingleObject(data->init_event, INFINITE);
-	D3DSURFACE_DESC desc;
-	data->capture_buffer->GetDesc(&desc);
-	RECT rect = { 0, 0, (int)desc.Width, (int)desc.Height };
-	HANDLE pipe = NULL;
 	while (true) {
-		while (pipe == NULL) {
-			Sleep(50);
-			pipe = connect(rect);
-		}
-		data->ready = true;
-		WaitForSingleObject(data->frame_event, INFINITE);
-		if (data->middle_buffer != NULL) {
-			if (FAILED(data->device->StretchRect(data->back_buffer, NULL, data->middle_buffer, NULL, D3DTEXF_NONE))) {
+		WaitForSingleObject(data->init_event, INFINITE);
+		D3DSURFACE_DESC desc;
+		data->capture_buffer->GetDesc(&desc);
+		RECT rect = { 0, 0, (int)desc.Width, (int)desc.Height };
+		HANDLE pipe = NULL;
+		while (data->device != NULL) {
+			if (pipe == NULL) {
+				Sleep(50);
+				pipe = connect(rect);
 				continue;
 			}
-			if (FAILED(data->device->GetRenderTargetData(data->middle_buffer, data->capture_buffer))) {
+			data->ready = true;
+			WaitForSingleObject(data->frame_event, INFINITE);
+			if (data->device == NULL) {
+				break;
+			}
+			if (data->middle_buffer != NULL) {
+				if (FAILED(data->device->StretchRect(data->back_buffer, NULL, data->middle_buffer, NULL, D3DTEXF_NONE))) {
+					continue;
+				}
+				if (FAILED(data->device->GetRenderTargetData(data->middle_buffer, data->capture_buffer))) {
+					continue;
+				}
+			}
+			else {
+				if (FAILED(data->device->GetRenderTargetData(data->back_buffer, data->capture_buffer))) {
+					continue;
+				}
+			}
+			if (FAILED(data->capture_buffer->LockRect(&data->rect, NULL, MAXDWORD))) {
 				continue;
 			}
-		}
-		else {
-			if (FAILED(data->device->GetRenderTargetData(data->back_buffer, data->capture_buffer))) {
-				continue;
+			DWORD bytes_to_write = desc.Width * desc.Height * 4;
+			DWORD bytes_written;
+			if (!WriteFile(pipe, data->rect.pBits, bytes_to_write, &bytes_written, NULL) || bytes_written != bytes_to_write) {
+				CloseHandle(pipe);
+				pipe = NULL;
 			}
+			data->capture_buffer->UnlockRect();
 		}
-		if (FAILED(data->capture_buffer->LockRect(&data->rect, NULL, MAXDWORD))) {
-			continue;
-		}
-		DWORD bytes_to_write = desc.Width * desc.Height * 4;
-		DWORD bytes_written;
-		if (!WriteFile(pipe, data->rect.pBits, bytes_to_write, &bytes_written, NULL) || bytes_written != bytes_to_write) {
+		if (pipe != NULL) {
 			CloseHandle(pipe);
-			pipe = NULL;
 		}
-		data->capture_buffer->UnlockRect();
 	}
 }
 
@@ -343,27 +404,37 @@ void d3d11_proc(D3D11_Data* data) {
 	}
 }
 
-uint8_t* write_hook(uint8_t* real_function, uint8_t* fake_function) {
+uint8_t* write_hook(uint8_t* fake_function, uint8_t* real_function, int patch_size_x86, int patch_size_x64) {
+	uint8_t push[1] = { 0x50 };
+	uint8_t pop[1] = { 0x58 };
 #ifdef _WIN64
-	uint8_t code[15] = { 0x48, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xE0 };
+	uint8_t code[] = { 0x48, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xE0 };
 	int offset = 2;
+	int patch_size = patch_size_x64;
 #else
-	uint8_t code[8] = { 0xB8, 0, 0, 0, 0, 0xFF, 0xE0 };
+	uint8_t code[] = { 0xB8, 0, 0, 0, 0, 0xFF, 0xE0 };
 	int offset = 1;
+	int patch_size = patch_size_x86;
 #endif
-	uint8_t* real_start = real_function + sizeof(code);
+	uint8_t* real_start = real_function + patch_size - sizeof(pop);
 	DWORD old_protect;
-	if (!VirtualProtect(real_function, sizeof(code), PAGE_EXECUTE_READWRITE, &old_protect)) {
+	if (!VirtualProtect(real_function, 1, PAGE_EXECUTE_READWRITE, &old_protect)) {
 		return NULL;
 	}
-	uint8_t* temp = (uint8_t*)VirtualAlloc(0, sizeof(code) * 2, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-	memcpy(temp, real_function, sizeof(code));
-	memcpy(temp + sizeof(code), code, sizeof(code));
-	memcpy(temp + sizeof(code) + offset, &real_start, sizeof(uint8_t*));
+	uint8_t* fake_start = (uint8_t*)VirtualAlloc(0, patch_size + sizeof(push) + sizeof(code), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	uint8_t* temp = fake_start;
+	memcpy(temp, real_function, patch_size);
+	temp += patch_size;
+	memcpy(temp, push, sizeof(push));
+	temp += sizeof(push);
+	memcpy(temp, code, sizeof(code));
+	memcpy(temp + offset, &real_start, sizeof(uint8_t*));
+	memset(real_function, 0x90, patch_size);
 	memcpy(real_function, code, sizeof(code));
 	memcpy(real_function + offset, &fake_function, sizeof(uint8_t*));
-	VirtualProtect(real_function, sizeof(uintptr_t), old_protect, &old_protect);
-	return temp;
+	memcpy(real_start, pop, sizeof(pop));
+	VirtualProtect(real_function, 1, old_protect, &old_protect);
+	return fake_start;
 }
 
 void opengl_init() {
@@ -374,7 +445,7 @@ void opengl_init() {
 	if (wgl_swap_buffers == NULL) {
 		return;
 	}
-	opengl_data.swap_buffers = write_hook((uint8_t*)wgl_swap_buffers, (uint8_t*)opengl_fake_swap_buffers);
+	opengl_data.swap_buffers = write_hook((uint8_t*)opengl_fake_swap_buffers, (uint8_t*)wgl_swap_buffers, 8, 15);
 	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)opengl_proc, &opengl_data, 0, NULL);
 }
 
@@ -398,8 +469,9 @@ void d3d9_init() {
 		return;
 	}
 	uintptr_t* vtable = *(uintptr_t**)device;
-	uint8_t* present = (uint8_t*)vtable[17];
-	d3d9_data.present = write_hook(present, (uint8_t*)d3d9_fake_present);
+	d3d9_data.release = write_hook((uint8_t*)d3d9_fake_release, (uint8_t*)vtable[2], 9, 17);
+	d3d9_data.reset = write_hook((uint8_t*)d3d9_fake_reset, (uint8_t*)vtable[16], 8, 18);
+	d3d9_data.present = write_hook((uint8_t*)d3d9_fake_present, (uint8_t*)vtable[17], 8, 15);
 	device->Release();
 	d3d->Release();
 	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)d3d9_proc, &d3d9_data, 0, NULL);
