a845f52aaa693b289d4bd326cfdec73a78885c2c
thealing <159199097+thealing@users.noreply.github.com>
2025-05-21 17:14:29 +0200

    Update MainSettingsDialog.cpp

--- Source/App/Views/MainSettingsDialog.cpp
+++ Source/App/Views/MainSettingsDialog.cpp
@@ -10,7 +10,7 @@ MainSettingsDialog::~MainSettingsDialog()
 
 DialogWindow* MainSettingsDialog::createWindow(Window* parent)
 {
-	return new DialogWindow(parent, L"Settings", 450, 28, 10, 6);
+	return new DialogWindow(parent, L"Settings", 450, 26, 10, 6);
 }
 
 void MainSettingsDialog::createControls(DialogWindow* window, MainSettings* settings)
@@ -22,20 +22,18 @@ void MainSettingsDialog::createControls(DialogWindow* window, MainSettings* sett
 	windowVisibilityNames[WindowVisibilityHidden] = L"Hidden";
 	window->addComboBox(L"Visibility during recording", 140, (int*)&settings->windowVisibility, windowVisibilityNames, ARRAYSIZE(windowVisibilityNames));
 	window->addSeparator();
+	window->addCheckBox(L"Stop recording when the source window changes", 16, &settings->stopWhenTheWindowChanges);
+	window->addCheckBox(L"Stop recording when an audio device changes", 16, &settings->stopWhenTheAudioDeviceChanges);
+	window->addCheckBox(L"Beep before the recording starts", 16, &settings->beepWhenTheRecordingStarts);
+	window->addCheckBox(L"Beep after the recording ends", 16, &settings->beepWhenTheRecordingStops);
+	window->addCheckBox(L"Ask to play recording when finished", 16, &settings->askToPlayTheSavedRecording);
+	window->addSeparator();
 	window->addCheckBox(L"High quality preview", 16, &settings->highQualityPreview);
 	window->addCheckBox(L"Disable preview while recording", 16, &settings->disablePreview);
 	window->addSeparator();
-	window->addCheckBox(L"Stop recording when the source window changes", 16, &settings->stopWhenTheWindowChanges);
-	window->addCheckBox(L"Stop recording when an audio device changes", 16, &settings->stopWhenTheAudioDeviceChanges);
-	window->addSeparator();
-	window->addCheckBox(L"Ask to play recording when finished", 16, &settings->askToPlayTheSavedRecording);
-	window->addSeparator();
-	window->addCheckBox(L"Use hardware acceleration", 16, &settings->useHardwareEncoder);
+	window->addCheckBox(L"Use hardware encoder", 16, &settings->useHardwareEncoder);
 	window->addCheckBox(L"Low latency mode", 16, &settings->lowLatencyEncoder);
 	window->addCheckBox(L"Disable throttling", 16, &settings->disableThrottling);
-	window->addSeparator();
-	window->addCheckBox(L"Beep before the recording starts", 16, &settings->beepWhenTheRecordingStarts);
-	window->addCheckBox(L"Beep after the recording ends", 16, &settings->beepWhenTheRecordingStops);
 	window->addSeparator();
 	window->addHotkeyEdit(L"Start recording", 120, &settings->startHotkey);
 	window->addHotkeyEdit(L"Stop recording", 120, &settings->stopHotkey);
