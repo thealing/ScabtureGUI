bbd3633b8bd0c1a9add6533bcc04b331baf40fbf
thealing <159199097+thealing@users.noreply.github.com>
2025-05-31 00:06:31 +0200

    Added throttling to recording display

--- Source/App/Controllers/RecordingPresenter.h
+++ Source/App/Controllers/RecordingPresenter.h
@@ -10,13 +10,17 @@ public:
 
 private:
 
+	void onEncodeEvent();
+
+	void onCaptureChange();
+
 	void update();
 
 	void reset();
 
 private:
 
-	WindowEventDispatcher _eventDispatcher;
+	EventDispatcher _eventDispatcher;
 	WeakPointer<RecordingDisplay> _recordingDisplay;
 	WeakPointer<RecordingManager> _recordingManager;
 	WeakPointer<VideoCaptureManager> _videoCaptureManager;
