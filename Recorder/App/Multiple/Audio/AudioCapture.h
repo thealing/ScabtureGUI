7ba5a09c3f8a18cad0bb49b40458238830111601
thealing <159199097+thealing@users.noreply.github.com>
2025-07-08 02:16:37 +0200

    Removed useless class

--- Recorder/App/Multiple/Audio/AudioCapture.h
+++ Recorder/App/Multiple/Audio/AudioCapture.h
@@ -1,9 +1,13 @@
 #pragma once
 
-class AudioCapture : public MediaSource
+class AudioCapture : public FrameSource
 {
 public:
 
 	virtual ~AudioCapture();
+
+	virtual HRESULT getFormat(IMFMediaType** format) = 0;
+
+	virtual HRESULT getSample(IMFSample** sample) = 0;
 };
 
