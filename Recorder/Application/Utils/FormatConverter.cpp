666b04bf3d1ebaec8131891b60c3abfc43424cf2
thealing <159199097+thealing@users.noreply.github.com>
2025-09-15 00:02:41 +0200

    Fixed const correctness

--- Recorder/Application/Utils/FormatConverter.cpp
+++ Recorder/Application/Utils/FormatConverter.cpp
@@ -34,7 +34,7 @@ void FormatConverter::convert_bgr_to_y(const uint32_t* bgr, uint8_t* y, __m128i 
 	{
 		for (size_t j = 0; j < 8; j++)
 		{
-			__m128i p = _mm_load_si128((__m128i*)(bgr + i + j * 4));
+			__m128i p = _mm_load_si128((const __m128i*)(bgr + i + j * 4));
 			w[j * 2] = _mm_cvtepu8_epi16(p);
 			w[j * 2 + 1] = _mm_cvtepu8_epi16(_mm_unpackhi_epi64(p, p));
 		}
@@ -72,8 +72,8 @@ void FormatConverter::convert_bgr_to_uv(const uint32_t* bgr, uint8_t* uv, __m128
 			__m128i w[16];
 			for (size_t j = 0; j < 8; j++)
 			{
-				__m128i p0 = _mm_load_si128((__m128i*)(bgr + r * width + c + j * 4));
-				__m128i p1 = _mm_load_si128((__m128i*)(bgr + (r + 1) * width + c + j * 4));
+				__m128i p0 = _mm_load_si128((const __m128i*)(bgr + r * width + c + j * 4));
+				__m128i p1 = _mm_load_si128((const __m128i*)(bgr + (r + 1) * width + c + j * 4));
 				p0 = _mm_avg_epu8(p0, p1);
 				p1 = _mm_shuffle_epi32(p0, _MM_SHUFFLE(2, 3, 0, 1));
 				p0 = _mm_avg_epu8(p0, p1);
@@ -116,8 +116,8 @@ void FormatConverter::convert_bgr_to_uv(const uint32_t* bgr, uint8_t* u, uint8_t
 			__m128i w[16];
 			for (size_t j = 0; j < 8; j++)
 			{
-				__m128i p0 = _mm_load_si128((__m128i*)(bgr + r * width + c + j * 4));
-				__m128i p1 = _mm_load_si128((__m128i*)(bgr + (r + 1) * width + c + j * 4));
+				__m128i p0 = _mm_load_si128((const __m128i*)(bgr + r * width + c + j * 4));
+				__m128i p1 = _mm_load_si128((const __m128i*)(bgr + (r + 1) * width + c + j * 4));
 				p0 = _mm_avg_epu8(p0, p1);
 				p1 = _mm_shuffle_epi32(p0, _MM_SHUFFLE(2, 3, 0, 1));
 				p0 = _mm_avg_epu8(p0, p1);
